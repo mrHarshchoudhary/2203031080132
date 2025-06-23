5e249738119a96437eb8e7633d2c2f0cae245b33
mrHarshchoudhary <Harshchoudharyv@gmail.com>
2025-06-23 14:06:56 +0530

    shorturl

--- backend/services/urlServices.js
+++ backend/services/urlServices.js
@@ -9,30 +9,3 @@ const createShortUrl = async (originalUrl) => {
   await url.save();
   return url;
 };
-
-// const Url = require('../models/url');
-// const shortid = require('shortid');
-
-// const createShortUrl = async (originalUrl) => {
-//   const shortId = shortid.generate();
-//   const url = new Url({
-//     originalUrl,
-//     shortId
-//   });
-//   await url.save();
-//   return url;
-// };
-
-// const getOriginalUrl = async (shortId) => {
-//   const url = await Url.findOneAndUpdate(
-//     { shortId },
-//     { $inc: { clicks: 1 } },
-//     { new: true }
-//   );
-//   return url?.originalUrl;
-// };
-
-// module.exports = {
-//   createShortUrl,
-//   getOriginalUrl
-// };
